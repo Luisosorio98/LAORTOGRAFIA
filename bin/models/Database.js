ab51c4d9e264d837787d56d27b8ce7f1f10b518a
Luisosorio98 <lo737744@gmail.com>
2019-07-15 20:29:58 +0000

    backend

--- bin/models/Database.js
+++ bin/models/Database.js
@@ -6,7 +6,7 @@
 
     try{
     await mongoose.connect(
-        "mongodb+srv://jeancarlos006:jeancarlos16fcb.@cluster0-wzhie.mongodb.net/SISTEMA?retryWrites=true&w=majority",
+        "mongodb+srv://luisosorio98:osorio2019#@cluster0-0mtsj.mongodb.net/laortografia?retryWrites=true&w=majority",
         {useNewUrlParser:true}
     );
     console.log('conectados a la base de datos!')
